a18f69ecdcc07388e57125e3c18efb38cf745916
Anton Gorbikov <anton.gorbikov@gmail.com>
2017-08-29 14:33:46 +0300

    Start server implementations

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// TODO:
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {};
+
+module.exports.init = (options) => {
+	// TODO: Copy required files to the host project.
+
+	// TODO: Validate that __dirname works for NPM package.
+	function getPath(resourcePath) {
+		return path.join(__dirname, options.root, resourcePath);
+	}
+
+	function getImage(request, response) {
+
+	}
+
+	function getData(request, response) {
+		let result = {};
+
+		options.testLists.map(getPath).forEach(function(testList) {
+			let data = JSON.parse(fs.readFileSync(testList));
+		});
+
+		response.send(result);
+	}
+
+	return {
+		report: () => {
+			const app = express();
+
+			app.use(express.static(path.join(__dirname, options.root)));
+			app.get('/image', getImage);
+			app.get('/data', getData);
+
+			app.listen(9001, () => {
+				console.log('http://localhost:9001');
+			});
+		}
+	};
+};
